434dbe0f9342fd85f661bb1c1adc5f257204975b
郑志成 <zhengzhicheng@zhengzhichengdemacbook-pro.local>
2016-01-11 20:13:18 +0800

    新建项目

---
+++ wangyiNes/wangyiNes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wangyiNes
+//
+//  Created by 郑志成 on 16/1/11.
+//  Copyright © 2016年 ZZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wangyiNes/wangyiNes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wangyiNes
+//
+//  Created by 郑志成 on 16/1/11.
+//  Copyright © 2016年 ZZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
