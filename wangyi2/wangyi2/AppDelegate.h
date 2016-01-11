ec0906adf9e18763ac7db86d74aa67ebc5ca6c19
郑志成 <zhengzhicheng@zhengzhichengdemacbook-pro.local>
2016-01-11 20:15:31 +0800

    新建项目

---
+++ wangyi2/wangyi2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wangyi2
+//
+//  Created by 郑志成 on 16/1/11.
+//  Copyright © 2016年 ZZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
